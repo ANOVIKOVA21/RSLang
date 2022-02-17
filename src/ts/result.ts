218eee9ec1c86587fa9be961ce9d4f35e2652645
ANOVIKOVA21 <anna2013novikova@gmail.com>
2022-02-17 23:47:11 +0200

    feat: add functions to show the result of the game

---
+++ src/ts/result.ts
@@ -0,0 +1,39 @@
+import { levelOptions, GetGameWord } from './options';
+function addListItemAndListers(gameWords: GetGameWord[], parent: HTMLDivElement) {
+  const words = levelOptions.words;
+  gameWords.forEach((obj, index) => {
+    const listItem = `<li class="result__list-item">
+        <button type="button" class="result__audio button"></button>
+        <p class="result__word">${words[obj.questionNum].word} - ${words[obj.questionNum].wordTranslate}</p>
+      </li>`;
+    parent.insertAdjacentHTML('beforeend', listItem);
+    const audioBtns = parent.querySelectorAll('.result__audio');
+    audioBtns[index].addEventListener('click', () => {
+      const url = `https://anna-learnenglish.herokuapp.com/${words[obj.questionNum].audio}`;
+      const audio = new Audio(url);
+      audio.play();
+    });
+  });
+}
+function showList(el: HTMLElement) {
+  el.classList.toggle('result__show-list');
+}
+export function showResult() {
+  const resultTemp = document.getElementById('result-page') as HTMLTemplateElement;
+  const resultEl = resultTemp.content.cloneNode(true) as HTMLDivElement;
+  const score = resultEl.querySelector('.result__score') as HTMLSpanElement;
+  const amountOfRightAnswers = resultEl.querySelector('.result__right') as HTMLSpanElement;
+  const amountOfWrongAnswers = resultEl.querySelector('.result__wrong') as HTMLSpanElement;
+  const rightAnswersList = resultEl.querySelector('.result__right-list') as HTMLDivElement;
+  const wrongAnswersList = resultEl.querySelector('.result__wrong-list') as HTMLDivElement;
+
+  score.innerHTML = document.querySelector('.sprint-game__score')?.innerHTML || '0';
+  amountOfRightAnswers.innerHTML = `${levelOptions.userRightAnswers.length}`;
+  amountOfWrongAnswers.innerHTML = `${levelOptions.userWrongAnswers.length}`;
+  amountOfRightAnswers.addEventListener('click', () => showList(rightAnswersList));
+  amountOfWrongAnswers.addEventListener('click', () => showList(wrongAnswersList));
+  document.body.appendChild(resultEl);
+  document.body.style.overflow = 'hidden';
+  addListItemAndListers(levelOptions.userRightAnswers, rightAnswersList);
+  addListItemAndListers(levelOptions.userWrongAnswers, wrongAnswersList);
+}
