82919c0069a1c5a9b6d1d24de53b82103d1b3d68
ANOVIKOVA21 <anna2013novikova@gmail.com>
2022-02-21 15:54:32 +0200

    feat: add function shuffle and improve function getArrRandomNumbers

--- src/ts/general-functions.ts
+++ src/ts/general-functions.ts
@@ -1,16 +1,22 @@
+import { GetWordsData } from './request';
 export function getRandomNum(min: number, max: number) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-export function getArrRandomNumbers(arrLength: number) {
+export function getArrRandomNumbers(arrLength: number, maxNum: number, exception?: number) {
   const numbers: number[] = [];
   for (let i = 0; i < 100; i++) {
     if (numbers.length === arrLength) break;
-    const randomNum = getRandomNum(0, 29);
-    if (numbers.every((num) => num !== randomNum)) {
+    const randomNum = getRandomNum(0, maxNum);
+
+    if (randomNum != exception && !numbers.includes(randomNum)) {
       numbers.push(randomNum);
-    } else continue;
+    }
   }
   return numbers;
+}
+
+export function shuffle(arr: GetWordsData[]) {
+  return arr.sort(() => Math.random() - 0.5);
 }
